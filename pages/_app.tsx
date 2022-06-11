c6b53740f0e2e3c7aa30c12964c876123db39645
hwanfront <kim015jh@gmail.com>
2022-06-11 19:39:30 +0900

    fix from relative path to absolute path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Link from 'next/link'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Link from 'next/link';
 
 const Menu = () => {
   return (
     <div>
       <ul>
-        <li><Link href="/">main</Link></li>
-        <li><Link href="/about">about</Link></li>
+        <li>
+          <Link href="/">main</Link>
+        </li>
+        <li>
+          <Link href="/about">about</Link>
+        </li>
       </ul>
     </div>
   );
-}
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <><Menu /><Component {...pageProps} /></>
+  return (
+    <>
+      <Menu />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
