973dc3d1cfd333f71b577b82acbf3237db60cd93
hwanfront <kim015jh@gmail.com>
2022-06-14 23:54:51 +0900

    add btn test layout page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import Button from '@/components/Button';
 import Header from '@/components/Header';
+import HeaderRight from '@/components/HeaderRight';
 import Logo from '@/components/Logo';
 import Search from '@/components/Search';
-import '@/styles/globals.css';
+import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header left={<Logo />} center={<Search />} right={<Button />} />
+      <Header left={<Logo />} center={<Search />} right={<HeaderRight />} />
       <Component {...pageProps} />
     </>
   );
---
+++ pages/component_test.tsx
@@ -0,0 +1,7 @@
+import TestLayout from '@/layout/TestLayout';
+
+const ComponentTest = () => {
+  return <TestLayout />;
+};
+
+export default ComponentTest;
