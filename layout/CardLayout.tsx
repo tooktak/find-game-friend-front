c628a3b2f4bf3002f89f3a94b41d54f7470718b2
hwanfront <kim015jh@gmail.com>
2022-06-19 22:31:41 +0900

    add responsive for Card Layout

--- layout/CardLayout.tsx
+++ layout/CardLayout.tsx
@@ -3,64 +3,66 @@ import GridLayout from '@/components/GridLayout';
 
 const CardLayout = () => {
   return (
-    <GridLayout>
-      <FindMatePostCard
-        thumbnail="/google-extention-architecture.png"
-        title="titleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee..."
-        kakaoLink="http://google.com"
-        discordLink="http://google.com"
-        content="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-        hashtags={['aaa', 'bbb']}
-      />
-      <FindMatePostCard
-        thumbnail="/test.jpg"
-        title="title..."
-        kakaoLink="http://google.com"
-        discordLink="http://google.com"
-        content="content..."
-        hashtags={['aaa', 'bbb']}
-      />
-      <FindMatePostCard
-        thumbnail="/logo.jpg"
-        title="title..."
-        kakaoLink="http://google.com"
-        discordLink="http://google.com"
-        content="content..."
-        hashtags={['aaa', 'bbb']}
-      />
-      <FindMatePostCard
-        thumbnail="/test1.png"
-        title="title..."
-        kakaoLink="http://google.com"
-        discordLink="http://google.com"
-        content="content..."
-        hashtags={['aaa', 'bbb']}
-      />
-      <FindMatePostCard
-        thumbnail="/test.jpg"
-        title="title..."
-        kakaoLink="http://google.com"
-        discordLink="http://google.com"
-        content="content..."
-        hashtags={['aaa', 'bbb']}
-      />
-      <FindMatePostCard
-        thumbnail="/test.jpg"
-        title="title..."
-        kakaoLink="http://google.com"
-        discordLink="http://google.com"
-        content="content..."
-        hashtags={['aaa', 'bbb']}
-      />
-      <FindMatePostCard
-        thumbnail="/test.jpg"
-        title="title..."
-        kakaoLink="http://google.com"
-        discordLink="http://google.com"
-        content="content..."
-        hashtags={['aaa', 'bbb']}
-      />
-    </GridLayout>
+    <main>
+      <GridLayout>
+        <FindMatePostCard
+          thumbnail="/google-extention-architecture.png"
+          title="titleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee..."
+          kakaoLink="http://google.com"
+          discordLink="http://google.com"
+          content="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+          hashtags={['aaa', 'bbb']}
+        />
+        <FindMatePostCard
+          thumbnail="/test.jpg"
+          title="title..."
+          kakaoLink="http://google.com"
+          discordLink="http://google.com"
+          content="content..."
+          hashtags={['aaa', 'bbb']}
+        />
+        <FindMatePostCard
+          thumbnail="/logo.jpg"
+          title="title..."
+          kakaoLink="http://google.com"
+          discordLink="http://google.com"
+          content="content..."
+          hashtags={['aaa', 'bbb']}
+        />
+        <FindMatePostCard
+          thumbnail="/test1.png"
+          title="title..."
+          kakaoLink="http://google.com"
+          discordLink="http://google.com"
+          content="content..."
+          hashtags={['aaa', 'bbb']}
+        />
+        <FindMatePostCard
+          thumbnail="/test.jpg"
+          title="title..."
+          kakaoLink="http://google.com"
+          discordLink="http://google.com"
+          content="content..."
+          hashtags={['aaa', 'bbb']}
+        />
+        <FindMatePostCard
+          thumbnail="/test.jpg"
+          title="title..."
+          kakaoLink="http://google.com"
+          discordLink="http://google.com"
+          content="content..."
+          hashtags={['aaa', 'bbb']}
+        />
+        <FindMatePostCard
+          thumbnail="/test.jpg"
+          title="title..."
+          kakaoLink="http://google.com"
+          discordLink="http://google.com"
+          content="content..."
+          hashtags={['aaa', 'bbb']}
+        />
+      </GridLayout>
+    </main>
   );
 };
 
