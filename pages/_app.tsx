e368710f0d24ae46f9b2536dfa9c8428bbcae377
hwanfront <kim015jh@gmail.com>
2022-06-19 22:30:07 +0900

    remove header Component for layout test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import Header from '@/components/Header';
-import HeaderRight from '@/components/HeaderRight';
-import Logo from '@/components/Logo';
-import Search from '@/components/Search';
+// import Header from '@/components/Header';
+// import HeaderRight from '@/components/HeaderRight';
+// import Logo from '@/components/Logo';
+// import Search from '@/components/Search';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header left={<Logo />} center={<Search />} right={<HeaderRight />} />
+      {/* <Header left={<Logo />} center={<Search />} right={<HeaderRight />} /> */}
       <Component {...pageProps} />
     </>
   );
