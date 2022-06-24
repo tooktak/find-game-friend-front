00cce8a9d4d18540f1b8d5a7aedd67956b21ee34
hwanfront <kim015jh@gmail.com>
2022-06-24 21:23:38 +0900

    add axios, react-query setting

---
+++ libs/client.ts
@@ -0,0 +1,13 @@
+import { QueryClient } from 'react-query';
+
+export const getClient = (() => {
+  let client: QueryClient | null = null;
+  return () => {
+    if (!client) client = new QueryClient();
+    return client;
+  };
+})();
+
+export const QueryKeys = {
+  FIND_MATE_POST: 'FIND_MATE_POST',
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import HeaderLayout from '@/layout/HeaderLayout';
 import '@/styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { getClient } from 'libs/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = getClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <HeaderLayout />
       <Component {...pageProps} />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
