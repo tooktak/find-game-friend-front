4198197cb9d49144f2fdeecbf6c9d0367775bcdd
hwanfront <kim015jh@gmail.com>
2022-06-24 21:18:26 +0900

    add findMatePost type

---
+++ types/type.d.ts
@@ -0,0 +1,11 @@
+type FindMatePost = {
+  id: string;
+  title: string;
+  contents: string;
+  hashtag: string[];
+  kakaoLink: string;
+  discordLink: string;
+  latestPullUpDateTime: string;
+  memberId: string;
+  gameId: string;
+};
