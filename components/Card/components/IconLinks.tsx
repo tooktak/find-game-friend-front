954af0f83cb2b8b5ca288e01248aeedad57ab567
hwanfront <kim015jh@gmail.com>
2022-06-27 00:51:09 +0900

    refactor Card

--- components/Card/components/IconLinks.tsx
+++ components/Card/components/IconLinks.tsx
@@ -1,19 +1,28 @@
 import { RiKakaoTalkFill } from 'react-icons/ri';
 import { SiDiscord } from 'react-icons/si';
 import styles from './IconLinks.module.scss';
+import cx from '@/styles/cx';
+
+// const classNames = cx(styles)('button', size, color, { outline, disabled });
 
 const DiscordLink = ({ link }: { link: string }) => {
+  const className = cx(styles)('linkContainer', 'discord');
   return (
-    <a className={styles.discordLink} href={link}>
-      <SiDiscord />
+    <a className={className} href={link}>
+      <div>
+        <SiDiscord />
+      </div>
     </a>
   );
 };
 
 const KakaoLink = ({ link }: { link: string }) => {
+  const className = cx(styles)('linkContainer', 'kakao');
   return (
-    <a className={styles.kakaoLink} href={link}>
-      <RiKakaoTalkFill />
+    <a className={className} href={link}>
+      <div>
+        <RiKakaoTalkFill />
+      </div>
     </a>
   );
 };
--- components/Card/components/index.tsx
+++ components/Card/components/index.tsx
@@ -1,3 +1,6 @@
+import { ReactNode } from 'react';
+import styles from './index.module.scss';
+
 import Thumbnail from './Thumbnail';
 import Title from './Title';
 import Hashtag from './Hashtag';
@@ -6,12 +9,16 @@ import Description from './Description';
 import ContentHeader from './ContentHeader';
 import Content from './Content';
 
-export {
-  Thumbnail,
-  Title,
-  Hashtag,
-  IconLinks,
-  Description,
-  ContentHeader,
-  Content,
+const Card = ({ children }: { children: ReactNode }) => {
+  return <div className={styles.card}>{children}</div>;
 };
+
+Card.Thumbnail = Thumbnail;
+Card.Title = Title;
+Card.Description = Description;
+Card.Hashtag = Hashtag;
+Card.IconLinks = IconLinks;
+Card.ContentHeader = ContentHeader;
+Card.Content = Content;
+
+export default Card;
