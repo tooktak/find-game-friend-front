a81b0f5091c3fed0f31f9618047e6b27ccaee97a
hwanfront <kim015jh@gmail.com>
2022-06-29 19:57:46 +0900

    fix Card component for skeleton

--- components/Card/components/Description.tsx
+++ components/Card/components/Description.tsx
@@ -1,10 +1,15 @@
+import { ReactNode } from 'react';
 import styles from './Description.module.scss';
+
+export const DescriptionContainer = ({ children }: { children: ReactNode }) => {
+  return <div className={styles.container}>{children}</div>;
+};
 
 const Description = ({ children }: { children: string }) => {
   return (
-    <div className={styles.container}>
+    <DescriptionContainer>
       <p className={styles.description}>{children}</p>
-    </div>
+    </DescriptionContainer>
   );
 };
 
--- components/Card/components/index.tsx
+++ components/Card/components/index.tsx
@@ -3,9 +3,9 @@ import styles from './index.module.scss';
 
 import Thumbnail from './Thumbnail';
 import Title from './Title';
-import Hashtag from './Hashtag';
+import Hashtag, { HashtagContainer } from './Hashtag';
 import IconLinks from './IconLinks';
-import Description from './Description';
+import Description, { DescriptionContainer } from './Description';
 import ContentHeader from './ContentHeader';
 import Content from './Content';
 
@@ -16,7 +16,9 @@ const Card = ({ children }: { children: ReactNode }) => {
 Card.Thumbnail = Thumbnail;
 Card.Title = Title;
 Card.Description = Description;
+Card.DescriptionContainer = DescriptionContainer;
 Card.Hashtag = Hashtag;
+Card.HashtagContainer = HashtagContainer;
 Card.IconLinks = IconLinks;
 Card.ContentHeader = ContentHeader;
 Card.Content = Content;
