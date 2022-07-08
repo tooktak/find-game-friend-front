ab2035e078d8ac824c61f6b600368cf406dfdf7d
hwanfront <kim015jh@gmail.com>
2022-07-08 21:21:02 +0900

    fix nextjs test api

--- pages/api/login.tsx
+++ pages/api/login.tsx
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-const getFindMatePost = (_: NextApiRequest, res: NextApiResponse) => {
+const handler = (_: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ id: 1 });
 };
 
-export default getFindMatePost;
+export default handler;
