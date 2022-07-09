bdcad29b2f096f9f3e103a13dd56dd2e633a3b23
hwanfront <kim015jh@gmail.com>
2022-07-09 23:49:53 +0900

    fix useFormChange

--- hooks/useFormChange.ts
+++ hooks/useFormChange.ts
@@ -3,13 +3,16 @@ import { ChangeEvent, useCallback, useState } from 'react';
 function useFormChange<T>(initialState: T) {
   const [form, setForm] = useState<T>(initialState);
 
-  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm(form => ({
-      ...form,
-      [name]: value,
-    }));
-  }, []);
+  const onChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+      const { name, value } = e.target;
+      setForm(form => ({
+        ...form,
+        [name]: value,
+      }));
+    },
+    [],
+  );
 
   return [form, onChange, setForm] as const;
 }
