d0717d3a3f64132f8a231c6cda6d9b66324467f4
hwanfront <kim015jh@gmail.com>
2022-07-17 19:33:25 +0900

    add game api

---
+++ services/game.ts
@@ -0,0 +1,36 @@
+import fetcher from '@/libs/fetcher';
+
+const findAll = async () => {
+  const response = await fetcher.get<Game[]>('/game');
+  return response.data;
+};
+
+const create = async ({ title, thumbnailURL }: Game) => {
+  const response = await fetcher.post<string>('/game', {
+    title,
+    thumbnailURL,
+  });
+  return response.data;
+};
+
+const update = async (id: string, { title, thumbnailURL }: Game) => {
+  const response = await fetcher.put<string>(`/game/${id}`, {
+    title,
+    thumbnailURL,
+  });
+  return response.data;
+};
+
+const remove = async (id: string) => {
+  const response = await fetcher.delete<string>(`/game/${id}`);
+  return response.data;
+};
+
+const findMatePost = {
+  findAll,
+  create,
+  update,
+  remove,
+};
+
+export default findMatePost;
