7e5d809faf397c6127e1ef58c96cacec8feb1670
hwanfront <kim015jh@gmail.com>
2022-07-29 20:18:12 +0900

    fix common Components

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,47 +1,42 @@
-import styles from './Button.module.scss';
+import { ButtonHTMLAttributes, forwardRef } from 'react';
 import cx from '@/styles/cx';
-import { forwardRef } from 'react';
+import styles from './Button.module.scss';
 
-type Props = {
-  children: string;
-  onClick?: () => void;
-  type?: 'submit';
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'large' | 'medium' | 'small';
   color?: 'main' | 'sub' | 'tag';
   rounded?: boolean;
   outline?: boolean;
   disabled?: boolean;
-};
+  children: string;
+}
 
-const Button = forwardRef<HTMLButtonElement, Props>(
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (
     {
-      children,
-      onClick,
-      type,
       size = 'medium',
       color,
       rounded = false,
       outline = false,
       disabled,
+      children,
+      ...rest
     },
     ref,
   ) => {
     const classNames = cx(styles)('button', size, color, {
+      rounded,
       outline,
       disabled,
     });
 
     return (
       <button
-        type={type}
+        type="button"
         className={classNames}
-        onClick={onClick}
         disabled={disabled}
         ref={ref}
-        style={{
-          borderRadius: rounded ? '50px' : '',
-        }}
+        {...rest}
       >
         {children}
       </button>
--- components/Input/TextInput.tsx
+++ components/Input/TextInput.tsx
@@ -1,34 +1,23 @@
 import cx from '@/styles/cx';
-import { forwardRef, memo } from 'react';
+import { forwardRef, InputHTMLAttributes, memo } from 'react';
 import styles from './TextInput.module.scss';
 
-export type TextInputProps = {
-  type?: string;
-  placeholder?: string;
-  name: string;
-  value: string;
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   bold?: boolean;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-};
+}
 
-const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
-  function TextInput({ name, placeholder, type, value, bold, onChange }, ref) {
+const TextInput = forwardRef<HTMLInputElement, Props>(
+  ({ name, bold, ...rest }, ref) => {
     const className = cx(styles)('input', { bold });
     return (
       <div className={styles.container}>
-        <input
-          ref={ref}
-          className={className}
-          type={type}
-          placeholder={placeholder ? placeholder : name}
-          name={name}
-          value={value}
-          onChange={onChange}
-        />
+        <input ref={ref} className={className} {...rest} />
         <label className={styles.label}>{name}</label>
       </div>
     );
   },
 );
+
+TextInput.displayName = 'TextInput';
 
 export default memo(TextInput);
