0cdeb933b55b5bf9f34cb0afaeea6835effae503
hwanfront <kim015jh@gmail.com>
2022-08-06 04:03:52 +0900

    fix error

--- components/Input/index.ts
+++ components/Input/index.ts
@@ -1,7 +1,6 @@
 import TextInput from './TextInput';
 import FileInput from './FileInput';
-import SearchInput, { SearchInputProps } from './SearchInput';
+import SearchInput from './SearchInput';
 import MultilineInput from './MultilineInput';
 
-export type { SearchInputProps };
 export { TextInput, FileInput, SearchInput, MultilineInput };
