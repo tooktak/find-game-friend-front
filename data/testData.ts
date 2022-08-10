877646a8a9c549e68f44d6fa559ea5d976cdee30
hwanfront <kim015jh@gmail.com>
2022-08-10 23:49:33 +0900

    fix nextjs test set api

--- data/testData.ts
+++ data/testData.ts
@@ -2,10 +2,23 @@ import fsPromises from 'fs/promises';
 import path from 'path';
 
 const getJsonData = async <T>(file: string) => {
-  const filePath = path.join(process.cwd(), `data/test/${file}`);
-  const jsonData = await fsPromises.readFile(filePath);
-  const members = JSON.parse(jsonData.toString()) as T;
-  return members;
+  try {
+    const filePath = path.join(process.cwd(), `data/test/${file}`);
+    const jsonData = await fsPromises.readFile(filePath);
+    const data = JSON.parse(jsonData.toString()) as T;
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const setJsonData = <T>(file: string, data: T) => {
+  try {
+    const filePath = path.join(process.cwd(), `data/test/${file}`);
+    return fsPromises.writeFile(filePath, JSON.stringify(data));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const testData = {
@@ -20,6 +33,15 @@ const testData = {
   getGames: async () => {
     const data = await getJsonData<Game[]>('game.json');
     return data;
+  },
+  setMembers: (data: Member[]) => {
+    return setJsonData('member.json', data);
+  },
+  setFindMatePosts: (data: FindMatePost[]) => {
+    return setJsonData('find-mate-post.json', data);
+  },
+  setGames: (data: Game[]) => {
+    return setJsonData('game.json', data);
   },
 };
 
