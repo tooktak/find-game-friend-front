b40b5e692e669805009aec03c1b2161471f4772f
dojunho <djh20@kumoh.ac.kr>
2023-03-10 01:17:21 +0900

    tmp

--- layout/LoginLayout.tsx
+++ layout/LoginLayout.tsx
@@ -1,23 +1,38 @@
 import Link from 'next/link';
-import { useMutation } from 'react-query';
+import {useMutation} from 'react-query';
 
-import { Layout } from '@/components/Layout';
-import { Button } from '@/components/Button';
-import { useLoginContext } from '@/context/Login';
+import {Layout} from '@/components/Layout';
+import {Button} from '@/components/Button';
+import {useLoginContext} from '@/context/Login';
 import memberService from '@/services/member';
-import GoogleLogin from "@leecheuk/react-google-login";
+import {useEffect, useRef} from "react";
+import {Http2ServerResponse} from "http2";
 
 const LoginLayout = () => {
-  const { setUserInfoData } = useLoginContext();
+  const {setUserInfoData} = useLoginContext();
 
-  const { mutate, isLoading } = useMutation(memberService.login, {
+  const {mutate, isLoading} = useMutation(memberService.login, {
     onSuccess: data => {
       setUserInfoData(data);
     },
   });
 
-  const onLogin = () => {
-    console.log("rere");
+  const useScript = (url: unknown, onload: unknown) => {
+    useEffect(() => {
+      const script = document.createElement('script');
+      script.src = url;
+      script.onload = onload;
+      document.head.appendChild(script);
+
+      return () => {
+        document.head.removeChild(script);
+      };
+    }, [url, onload]);
+  };
+
+  const googleSignInButton = useRef(null);
+  const onGoogleSignIn = (res) => {
+    console.log(res);
     const login = {
       memberId: 'asdfg',
       password: '0000',
@@ -25,18 +40,21 @@ const LoginLayout = () => {
     mutate(login);
   };
 
+  useScript('https://accounts.google.com/gsi/client', () => {
+    window.google.accounts.id.initialize({
+      client_id: "1033931690858-58kuqhjo9877bcnod4og9jg1av9cusk1.apps.googleusercontent.com",
+      callback: onGoogleSignIn,
+    });
+    window.google.accounts.id.renderButton(googleSignInButton.current, {
+      width: '250',
+      type: 'icon',
+      shape: 'circle',
+    });
+  });
+
   return (
     <Layout>
-      <Link href="/" passHref>
-        <Button onClick={onLogin} disabled={isLoading}>
-          {isLoading ? '로그인 중...' : '로그인'}
-        </Button>
-      </Link>
-      <GoogleLogin
-          clientId="1033931690858-58kuqhjo9877bcnod4og9jg1av9cusk1.apps.googleusercontent.com"
-          onSuccess={onLogin}
-          isSignedIn={true}
-      />
+      <div id="google-login-api" ref={googleSignInButton}></div>
     </Layout>
   );
 };
