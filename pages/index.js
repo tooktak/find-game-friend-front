dae7a17af06a74b24a89f45b5a21fec5b1df8e9a
boseok <asdww6631@gmail.com>
2023-05-04 18:55:30 +0900

    deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/dist/client/router';
 
 export default function Index() {
     const router = useRouter();
