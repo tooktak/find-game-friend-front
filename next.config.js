9b42c2c489ed5fe40479817b1da072ff8e6f2a03
boseok <asdww6631@gmail.com>
2023-05-12 18:49:25 +0900

    CSP 설정

--- next.config.js
+++ next.config.js
@@ -38,4 +38,20 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+          },
+        ],
+      },
+    ];
+  },
+};
