2a8b93b173f782af7e72ba4798c9d86d5ecc6016
boseok <asdww6631@gmail.com>
2023-05-19 19:48:57 +0900

    sameSite

--- libs/fetcher.ts
+++ libs/fetcher.ts
@@ -7,7 +7,7 @@ const fetcher = axios.create({
   timeout: 1000,
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'https://find-game-friend-front.vercel.app',
+    'Access-Control-Allow-Origin': 'https://aribomy.com',
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     'Access-Control-Allow-Credentials': true,
   },
