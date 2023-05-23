f5a9e18358538f3e148047cb2e46ea650b56002e
boseok <asdww6631@gmail.com>
2023-05-24 03:42:27 +0900

    storage에 쿠기 저장

--- layout/LoginLayout.tsx
+++ layout/LoginLayout.tsx
@@ -2,52 +2,43 @@ import { Layout } from '@/components/Layout';
 import { useLoginContext } from '@/context/Login';
 import memberService from '@/services/member';
 import { useEffect, useRef } from 'react';
-import { useMutation } from 'react-query';
 import { useRouter } from 'next/dist/client/router';
 
 const LoginLayout = () => {
   const { setUserInfoData } = useLoginContext();
   const router = useRouter();
-  const { mutate, isLoading } = useMutation(
-    memberService.google_oauth_callback,
-    {
-      onSuccess: data => {
-        setUserInfoData({ sub: data });
-      },
-    },
-  );
-
-  const useScript = (url: unknown, onload: unknown) => {
-    useEffect(() => {
-      const script = document.createElement('script');
-      script.src = url;
-      script.onload = onload;
-      document.head.appendChild(script);
-
-      return () => {
-        document.head.removeChild(script);
-      };
-    }, [url, onload]);
-  };
-
   const googleSignInButton = useRef(null);
+
   const onGoogleSignIn = async res => {
-    mutate(res.credential);
-    router.push('/', undefined, { shallow: true });
-    // 여기에 리턴값 확인 한뒤 맴버 정보 및 localstorge에 관련값 저장.
+    try {
+      const { credential } = res;
+      await memberService.google_oauth_callback(credential);
+      router.push('/', undefined, { shallow: true });
+    } catch (error) {
+      console.error('Google Sign-In Error:', error);
+    }
   };
 
-  useScript('https://accounts.google.com/gsi/client', () => {
-    window.google.accounts.id.initialize({
-      client_id:
-        '1033931690858-58kuqhjo9877bcnod4og9jg1av9cusk1.apps.googleusercontent.com',
-      callback: onGoogleSignIn,
-    });
-    window.google.accounts.id.renderButton(googleSignInButton.current, {
-      width: '250',
-      shape: 'square',
-    });
-  });
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://accounts.google.com/gsi/client';
+    script.onload = () => {
+      window.google.accounts.id.initialize({
+        client_id:
+          '1033931690858-58kuqhjo9877bcnod4og9jg1av9cusk1.apps.googleusercontent.com',
+        callback: onGoogleSignIn,
+      });
+      window.google.accounts.id.renderButton(googleSignInButton.current, {
+        width: '250',
+        shape: 'square',
+      });
+    };
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
 
   return (
     <Layout>
